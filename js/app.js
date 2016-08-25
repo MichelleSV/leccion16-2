b12241c1089c806287160b1b85c5845d98a26706
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-25 13:52:05 -0500

    corrigiendo js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 var num1 = parseInt(prompt("Escribe un número"));
 var num2 = parseInt(prompt("Escribe un número"));
-var num3 = parseInt(prompt("Escribe un número"))
+var num3 = parseInt(prompt("Escribe un número"));
 document.write((num1 + num2 + num3)/3);
